32961b98880e66c7c49c9758297e5eaea32830dd
Yreish Pozo <yreishmiguel@gmail.com>
2023-08-13 11:16:19 -0700

    Fine tuning

--- app_name/src/components/Services.jsx
+++ app_name/src/components/Services.jsx
@@ -3,32 +3,32 @@ import { BiSearchAlt } from "react-icons/bi";
 import { RiHeart2Fill } from "react-icons/ri";
 
 const ServiceCard = ({ color, title, icon, subtitle }) => (
-  <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+  <div className="flex flex-row items-center justify-start p-3 m-2 cursor-pointer white-glassmorphism hover:shadow-xl">
     <div
       className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}
     >
       {icon}
     </div>
-    <div className="ml-5 flex flex-col flex-1">
-      <h1 className="mt-2 text-white text-lg">{title}</h1>
-      <p className="mt-2 text-white text-sm md:w-9/12">{subtitle}</p>
+    <div className="flex flex-col flex-1 ml-5">
+      <h1 className="mt-2 text-lg text-white">{title}</h1>
+      <p className="mt-2 text-sm text-white md:w-9/12">{subtitle}</p>
     </div>
   </div>
 );
 
 const Services = () => {
   return (
-    <div className="flex flex-col md:flex-row w-full justify-center items-center gradient-bg-services">
-      <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
-        <div className="flex-1 flex flex-col justify-start items-start">
-          <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">
+    <div className="flex flex-col items-center justify-center w-full md:flex-row gradient-bg-services">
+      <div className="flex flex-col items-center justify-between px-4 py-12 mf:flex-row md:p-20">
+        <div className="flex flex-col items-start justify-start flex-1">
+          <h1 className="py-2 text-3xl text-white sm:text-5xl text-gradient">
             Services that we
             <br />
             continue to improve
           </h1>
         </div>
       </div>
-      <div className="flex-1 flex flex-col justify-start items-center">
+      <div className="flex flex-col items-center justify-start flex-1">
         <ServiceCard
           color="bg-[#2952E3]"
           title="Security guaranteed"
