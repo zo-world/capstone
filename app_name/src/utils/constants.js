e5fa0c2be88a9464cc5ca9df1fbbb7a7a67c646d
Weeraphot Bumbaugh <weeraphot@bumbaugh.com>
2023-08-20 10:59:12 -0700

    Fixed transactions

--- app_name/src/utils/constants.js
+++ app_name/src/utils/constants.js
@@ -1,4 +1,7 @@
 import abi from "./Transactions.json";
 
+//Make sure to get Transactions.json from smart_contract > artifacts > contracts > Transactions.json
 export const contractABI = abi.abi;
-export const contractAddress = "0x69346C9f307d75847806eF9930530030c9166c4D";
+
+// In terminal: npx hardhat run scripts/deploy.js --network sepolia. This is the deployed address of our contract
+export const contractAddress = "0x3973177CCe3d532f1dF41e33521220520B2f6709";
